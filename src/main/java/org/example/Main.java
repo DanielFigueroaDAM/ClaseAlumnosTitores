f804ded418215e0bfe7a348ed17b68d393fdcdfb
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-12-02 11:27:02 +0100

    avanzando en la main, falta el properties

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 import java.util.List;
 
+@org.springframework.boot.autoconfigure.SpringBootApplication
+@ComponentScan({"org.example"})
 public class Main {
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class, args);
     }
 }
